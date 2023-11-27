bf8ce4cb8c856fe17620ec162f7d40cfd3e6d89a
javiardid <javier.ardiddiaz@cieep.com>
2023-11-27 11:25:42 +0100

    Initial commit

---
+++ app/src/main/java/javi/ardid/sqlite/MainActivity.java
@@ -0,0 +1,42 @@
+package javi.ardid.sqlite;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+
+
+import javi.ardid.sqlite.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
+
+}
